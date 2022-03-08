4c3c42875de78ee5bc16beca9d4fd34a5de7b0fd
ikaverin <kaverin1990@gmail.com>
2022-03-09 01:08:28 +0300

    add exit button

--- src/components/Chat/components/UserPanel/index.jsx
+++ src/components/Chat/components/UserPanel/index.jsx
@@ -1,16 +1,26 @@
 import "./style.css";
+import { useNavigate } from 'react-router-dom';
 
 const UserPanel = ({ users }) => {
+
+  const navigate = useNavigate();
+
+
+  const handleLogout = () => {
+    navigate('/')
+  }
+
   return (
     <>
       <div className="tui-panel">
         <div className="tui-panel-header user-panel-header">
-          Online {users.length}
+          <span>Online {users.length}</span>
+          <button onClick={handleLogout} className="user-panel-button">Exit</button>
         </div>
         <div className="tui-panel-content">
           <ul>
             {users.map((user, index) => (
-              <li className="user" key={index}>
+              <li className="user-panel-user" key={index}>
                 {user}
               </li>
             ))}
